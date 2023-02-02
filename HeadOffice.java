71abdb008ab9b26683cd699f59e748589e1d9a3e
dhinakaran s <53988061+dhinakaran011@users.noreply.github.com>
2023-02-02 17:06:50 +0530

    HeadOffice.java

---
+++ HeadOffice.java
@@ -0,0 +1,11 @@
+abstract class HeadOffice{
+int amount;
+public static void main(String args[]){}
+  public void check_accounts(int amount){
+  System.out.println("Account Balance "+amount);
+  }
+  public int pay_tax(int amount){
+      return this.amount = amount;
+  }
+  public abstract void receive_Customers();
+}
